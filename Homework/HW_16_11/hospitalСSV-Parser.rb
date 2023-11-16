6e6fe4deb6ec9bc11263c41fd8008e541aafdddd
Oleksandr-Kornieiev <kornieiev2021ks11@student.karazin.ua>
2023-11-16 11:27:27 +0200

    adding new field to a csv file

--- Homework/HW_16_11/hospitalСSV-Parser.rb
+++ Homework/HW_16_11/hospitalСSV-Parser.rb
@@ -9,12 +9,13 @@ hospital_id = 1
 # Open CSV file in write mode
 CSV.open('hospitals.csv', 'w') do |row|
   # Write header to CSV file
-  row << ['ID', 'Hospital Name']
-  # Search for nodes by css
+  row << ['ID', 'Hospital Name', 'URL']
+  # Search for nodes by css in "div.responsive div#MainContainerInner section.content div.columnWrapper div.column1"
   doc.css('div#BlinkDBContent_849210 ul li a').each do |hyperlink_tag|
     hospital_name = hyperlink_tag.text.strip
-    # Add the hospital name and identifier to the CSV file
-    row << [hospital_id, hospital_name]
+    hospital_info = hyperlink_tag['href']
+    # Add the hospital name, link and identifier to the CSV file
+    row << [hospital_id, hospital_name, hospital_info]
     hospital_id += 1
   end
 end
